90a85aca79e6bcf54af13e56fce1b8fe51ba6bbf
Josh Hill <josh.hill@outlook.es>
2019-05-02 13:52:14 -0400

    Commit

--- Ecocabins.web/Pages/Contact.cshtml.cs
+++ Ecocabins.web/Pages/Contact.cshtml.cs
@@ -1,18 +1,226 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Ecocabins.web.Pages
+
+namespace Ecocabins.Web.Pages
 {
     public class ContactModel : PageModel
     {
+        public void OnGet() { }
+
+        [BindProperty]
+        [Required]
+        public string Name { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Address { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string City { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string State { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Zip { get; set; }
+
+        [BindProperty]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Phone { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Way { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Time { get; set; }
+ 
+        [BindProperty]
+        [Required]
+        public string Plan { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Land { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Finance { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Budget { get; set; }
+
+
+        [BindProperty]
+        [Required]
         public string Message { get; set; }
 
-        public void OnGet()
+
+  
+
+        public JsonResult OnPostContactFormSubmit()
         {
-            Message = "Your contact page.";
+            try
+            {
+                var mail = new MailMessage(
+                new MailAddress("josh.hill@outlook.es", "Joshua Hill"), // Needs to change to site's actual domain
+                new MailAddress("josh.hill@outlook.es", "Josh Hill")); // Change to Sarah's dad
+                mail.IsBodyHtml = true;
+
+                SmtpClient smtpClient = new SmtpClient("smtp-mail.outlook.com")
+                {
+                    UseDefaultCredentials = false,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new NetworkCredential("josh.hill@outlook.es", "83x1LIXsIcH7"),
+                    Port = 587,
+                    EnableSsl = true
+                };
+
+                mail.Subject = "Contact Form Submission";
+
+                // Build body
+                mail.Body = $@"<p>Contact form submission received on { DateTime.Now.ToString("MM/dd/yyyy") } @ { DateTime.Now.ToString("h:mm tt") }</p>
+                                <table>
+                                <tr>
+                                    <td>
+                                        <strong>Name</strong>
+                                    </td>
+                                    <td>
+                                        {Name}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <strong>Address</strong>
+                                    </td>
+                                    <td>
+                                        {Address}
+                                    </td>
+                                </tr>                      
+                                <tr>
+                                    <td>
+                                        <strong>City</strong>
+                                    </td>
+                                    <td>
+                                        {City}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <strong>State</strong>
+                                    </td>
+                                    <td>
+                                        {State}
+                                    </td>
+                                </tr>
+                                    <tr>
+                                    <td>
+                                        <strong>Zip Code</strong>
+                                    </td>
+                                    <td>
+                                        {Zip}
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>
+                                        <strong>Email</strong>
+                                    </td>
+                                    <td>
+                                       <a href='mailto:{Email}'>{Email}</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <strong>Phone</strong>
+                                    </td>
+                                    <td>
+                                        {Phone}
+                                    </td>
+                                </tr> 
+                                <tr>
+                                    <td>
+                                        <strong>Best way to contact</strong>
+                                    </td>
+                                    <td>
+                                        {Way}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <strong>Best time to contact</strong>
+                                    </td>
+                                    <td>
+                                        {Time}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <strong>Building timeframe</strong>
+                                    </td>
+                                    <td>
+                                        {Plan}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <strong>Land owned</strong>
+                                    </td>
+                                    <td>
+                                        {Land}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <strong>Financing</strong>
+                                    </td>
+                                    <td>
+                                        {Finance}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <strong>Budget</strong>
+                                    </td>
+                                    <td>
+                                        {Budget}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <strong>Message</strong>
+                                    </td>
+                                    <td>
+                                        {Message}
+                                    </td>
+                                </tr>
+
+            
+                            </table>";
+
+                smtpClient.Send(mail);
+                return new JsonResult(true);
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(false);
+            }
         }
     }
 }
---
+++ Ecocabins.web/wwwroot/js/contact.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(function () {
+    // Capture AJAX start/end
+    $body = $("body");
+
+    $(document).on({
+        ajaxStart: function () { $body.addClass("loading"); },
+        ajaxStop: function () { $body.removeClass("loading"); }
+    });
+
+    // Handle contact form submission
+    $("#frmContactForm").submit(function (e) {
+        // Cache this
+        const $this = $(this);
+
+        // Prevent form submission
+        e.preventDefault();
+
+        // Check validity of form
+        const isFormValid = $this.valid();
+
+        if (isFormValid) {
+            // Submit contact form via AJAX to server
+            $.ajax({
+                url: $this.attr('action'),
+                type: $this.attr('method'),
+                dataType: 'json',
+                data: $this.serialize(),
+                success: function (data) {
+                    if (data === true) {
+                        $("#contactDiv").addClass("d-none");
+                        $("#contactResult").html("Thank you for your submission. Someone will contact you shortly.").removeClass("d-none someredclass");
+                    } else {
+                        showContactError();
+                    }
+                },
+                error: function (xhr, err) {
+                    showContactError();
+                }
+            });
+        }
+
+        return false;
+    });
+});
+
+function showContactError() {
+    $("#contactResult").html("Sorry, an error has occurred. Please try again later.").addClass("someredclass").removeClass("d-none");
+}
